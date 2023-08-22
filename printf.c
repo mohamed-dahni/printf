ddd00be6b7908c4924c55e2463f181b295cf0742
Mohamed <mdahni@gmail.com>
2023-08-22 23:35:03 +0100

    +

--- printf.c
+++ printf.c
@@ -35,6 +35,11 @@ int _printf(char *format, ...)
 				s = va_arg(args, char*);
 				write(STDOUT_FILENO, s, _strlen(s));
 				bytes += _strlen(s);
+			} else if (*format == '%')
+			{
+				c = *format;
+				write(STDOUT_FILENO, &c, sizeof(char));
+				bytes += sizeof(char);
 			}
 		} else
 		{
